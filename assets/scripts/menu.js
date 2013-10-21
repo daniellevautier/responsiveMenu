429f4608520f231d071593031d71894e7e7c1de1
Danielle Vautier <danielle.vautier@cubeworks.co.uk>
2013-10-21 17:00:13 +0100

    lag fix with JS

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -3,24 +3,17 @@ Responsive Mobile Menu v1.0
 Author: Danielle Vautier
 */
 
-function responsiveMenu2() {	
-	$('.mainMenu ul').addClass("responsiveMenu"); //Add ul class for JS enabled 
-	
-	$('.mainMenu').prepend("<div class='menu-title'><h1>Menu</h1></div>" ); //Add menu title
-	
-	$('.mainMenu ul').hide(); //Hide menu on load
-	
-	$('.menu-title').click(function () {
-		$('.mainMenu ul').toggle(); 
+function responsiveMenu() {	
+	$('.toggle').click(function () {
+		$('.menu').slideToggle("slow");
+		$('.menu').css({"max-height":"800px"});
 	});
 	
 };
 
-function responsiveMenu() {	
-	$("#toggle").removeAttr('checked');
-};
-//If elsewhere on the  page is clicked hide menu
-
 $(function() {
-	$("#toggle").removeAttr('checked');
+	$(".menu").css({"display":"none","transition":"none","max-height":"inherit"});
+	$("#toggle").hide();
+	responsiveMenu();
 });
+
