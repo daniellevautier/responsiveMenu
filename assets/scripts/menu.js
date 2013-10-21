821148bed28fd5ca1892a943ed11f820a9254547
Danielle Vautier <danielle.vautier@cubeworks.co.uk>
2013-10-21 16:36:34 +0100

    menu

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -3,26 +3,24 @@ Responsive Mobile Menu v1.0
 Author: Danielle Vautier
 */
 
-function responsiveMenu() {	
-	$('.menu ul').addClass("responsiveMenu"); //Add ul class for JS enabled 
+function responsiveMenu2() {	
+	$('.mainMenu ul').addClass("responsiveMenu"); //Add ul class for JS enabled 
 	
-	$('.menu').prepend("<div class='menu-title'><h1>Menu</h1></div>" ); //Add menu title
+	$('.mainMenu').prepend("<div class='menu-title'><h1>Menu</h1></div>" ); //Add menu title
 	
-	$('.menu ul').hide(); //Hide menu on load
+	$('.mainMenu ul').hide(); //Hide menu on load
 	
 	$('.menu-title').click(function () {
-		$('.menu ul').toggle(); 
+		$('.mainMenu ul').toggle(); 
 	});
 	
 };
 
+function responsiveMenu() {	
+	$("#toggle").removeAttr('checked');
+};
 //If elsewhere on the  page is clicked hide menu
-$(document).click(function(event) {
-    if (!$(event.target).closest(".menu-title,.menu ul").length) {
-          $(".menu ul").hide();
-    }
-});
 
 $(function() {
-	responsiveMenu();
+	$("#toggle").removeAttr('checked');
 });
